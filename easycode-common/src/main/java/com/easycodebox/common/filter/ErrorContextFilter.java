85721e1284b87bce42f8257ff6ea43fc78fcb732
WangXiaoJin <381954728@qq.com>
2016-09-29 23:32:14 +0800

    update

--- easycode-common/src/main/java/com/easycodebox/common/filter/ErrorContextFilter.java
+++ easycode-common/src/main/java/com/easycodebox/common/filter/ErrorContextFilter.java
@@ -8,15 +8,11 @@ import java.util.Map;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.easycodebox.common.BaseConstants;
 import com.easycodebox.common.enums.Enums;
@@ -50,8 +46,6 @@ public class ErrorContextFilter implements Filter {
 	
 	private final String errorKey = "CODE_MSG";
 	private final String storeExceptionKey = "EXCEPTION_INFO";
-	
-	private WebApplicationContext context;
 	
 	/**
 	 * 检查异常类型的嵌套层次数。因为某些框架会把抛出的异常封装进cause属性中。
@@ -95,7 +89,7 @@ public class ErrorContextFilter implements Filter {
 	
 	private boolean storeException = false;
 	
-	private ExceptionHandler exceptionHandler;
+	protected ExceptionHandler exceptionHandler;
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
@@ -106,8 +100,7 @@ public class ErrorContextFilter implements Filter {
 				logMappings = filterConfig.getInitParameter("logMappings"),
 				isLog = filterConfig.getInitParameter("isLog"),
 				logLevel = filterConfig.getInitParameter("logLevel"),
-				store = filterConfig.getInitParameter("storeException"),
-				exceptionHandlerBeanName = filterConfig.getInitParameter("exceptionHandlerBeanName");
+				store = filterConfig.getInitParameter("storeException");
 		
 		if (StringUtils.isNotBlank(defaultPage)) {
 			this.defaultPage = defaultPage.trim();
@@ -171,21 +164,12 @@ public class ErrorContextFilter implements Filter {
 		if (StringUtils.isNotBlank(store)) {
 			this.storeException = Boolean.parseBoolean(store.trim());
 		}
-		if (StringUtils.isNotBlank(exceptionHandlerBeanName)) {
-			initWebContext(filterConfig.getServletContext());
-			exceptionHandler = context.getBean(exceptionHandlerBeanName.trim(), ExceptionHandler.class);
-		}
+		
 	}
 	
 	@Override
 	public void destroy() {
 		
-	}
-	
-	private void initWebContext(ServletContext sc) {
-		if (context == null) {
-			context = WebApplicationContextUtils.getRequiredWebApplicationContext(sc);
-		}
 	}
 	
 	@SuppressWarnings("unchecked")
