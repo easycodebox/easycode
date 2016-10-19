ffca3f0786ff25b79ef448333c2e1e23829545fb
WangXiaoJin <381954728@qq.com>
2016-10-19 21:19:52 +0800

    update

--- easycode-auth-core/src/test/java/com/easycodebox/auth/core/service/user/RoleServiceTest.java
+++ easycode-auth-core/src/test/java/com/easycodebox/auth/core/service/user/RoleServiceTest.java
@@ -1,29 +1,33 @@
 package com.easycodebox.auth.core.service.user;
 
-import org.junit.Test;
-
-import com.easycodebox.auth.core.pojo.user.Role;
 import com.easycodebox.auth.core.util.test.BaseTest;
-import com.easycodebox.common.enums.entity.status.CloseStatus;
 
 public class RoleServiceTest extends BaseTest<RoleService> {
-
+	/*
 	@Test
 	public void testLoad() {
-		LOG.info(bean.load(3859));
+		LOG.info(bean.load(8859));
+	}
+	
+	@Test
+	public void testAdd() {
+		Role role = new Role();
+		role.setName("test:" + RandomUtils.nextInt(1000));
+		role.setSort(0);
+		role = bean.add(role);
+		LOG.info(role);
 	}
 	
 	@Test
 	public void testUpdate() {
 		Role role = new Role();
-		role.setId(3859);
-		role.setName("管理员");
+		role.setId(8859);
+		role.setName("test:" + RandomUtils.nextInt(1000));
 		role.setSort(9);
-		role.setStatus(CloseStatus.OPEN);
 		role.setDescription("xxx");
 		LOG.info(bean.update(role));
 	}
-	
+	*/
 	/*
 	public static void main(String[] args) throws Exception {
 		System.out.println(ConfigTools.encrypt("root"));
