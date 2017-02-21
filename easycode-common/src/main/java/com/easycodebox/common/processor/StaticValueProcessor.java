8c649d2a7bf484a686c3267784a8d246ffbf8daf
WangXiaoJin <381954728@qq.com>
2017-02-21 16:19:26 +0800

    update

--- easycode-common/src/main/java/com/easycodebox/common/processor/StaticValueProcessor.java
+++ easycode-common/src/main/java/com/easycodebox/common/processor/StaticValueProcessor.java
@@ -1,18 +1,18 @@
 package com.easycodebox.common.processor;
 
+import com.easycodebox.common.error.BaseException;
+import com.easycodebox.common.file.Resources;
+import com.easycodebox.common.lang.DataConvert;
+import com.easycodebox.common.lang.Strings;
+import com.easycodebox.common.lang.reflect.Fields;
+import com.easycodebox.common.validate.Assert;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.List;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import com.easycodebox.common.error.BaseException;
-import com.easycodebox.common.file.Resources;
-import com.easycodebox.common.lang.DataConvert;
-import com.easycodebox.common.lang.reflect.Classes;
-import com.easycodebox.common.lang.reflect.Fields;
-import com.easycodebox.common.validate.Assert;
 
 /**
  * 根据properties属性值刷入Class的静态属性中，如果Class静态属性有初始化值，且properties中没有，则会反刷进properties中
@@ -29,7 +29,7 @@ public class StaticValueProcessor implements Processor {
 	private final Pattern pattern = Pattern.compile("^\\s*\\$\\s*\\{\\s*([\\w\\._\\-]+)\\s*\\}\\s*$");
 	
 	private String[] packagesToScan;
-	private String[] classes;
+	private Class[] classes;
 
 	private Properties properties;
 	
@@ -42,8 +42,8 @@ public class StaticValueProcessor implements Processor {
 				processStaticValue(clazz);
 			}
 			if(classes != null) {
-				for(String clazz : classes) {
-					processStaticValue(Classes.getClass(clazz));
+				for(Class clazz : classes) {
+					processStaticValue(clazz);
 				}
 			}
 		} catch (Exception e) {
@@ -62,7 +62,7 @@ public class StaticValueProcessor implements Processor {
 			StaticValue value = field.getAnnotation(StaticValue.class);
 			if(value != null) {
 				String str = value.value();
-				if(str != null) {
+				if(Strings.isNotBlank(str)) {
 					Matcher m = pattern.matcher(str);
 					if(m.find()) {
 						String key = m.group(1),
@@ -89,15 +89,15 @@ public class StaticValueProcessor implements Processor {
 	public void setPackagesToScan(String[] packagesToScan) {
 		this.packagesToScan = packagesToScan;
 	}
-
-	public String[] getClasses() {
+	
+	public Class[] getClasses() {
 		return classes;
 	}
-
-	public void setClasses(String[] classes) {
+	
+	public void setClasses(Class[] classes) {
 		this.classes = classes;
 	}
-
+	
 	public Properties getProperties() {
 		return properties;
 	}
