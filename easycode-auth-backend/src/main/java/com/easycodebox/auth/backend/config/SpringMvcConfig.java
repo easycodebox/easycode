0626f04d61159a5ee61cb724c46884bd1b9de510
WangXiaoJin <381954728@qq.com>
2017-03-28 17:24:59 +0800

    update

--- easycode-auth-backend/src/main/java/com/easycodebox/auth/backend/config/SpringMvcConfig.java
+++ easycode-auth-backend/src/main/java/com/easycodebox/auth/backend/config/SpringMvcConfig.java
@@ -2,14 +2,19 @@ package com.easycodebox.auth.backend.config;
 
 import com.easycodebox.common.web.springmvc.DefaultRequestMappingHandlerAdapter;
 import com.easycodebox.common.web.springmvc.DefaultRequestMappingHandlerMapping;
+import freemarker.ext.jsp.TaglibFactory.ClasspathMetaInfTldSource;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.web.WebMvcRegistrations;
 import org.springframework.boot.autoconfigure.web.WebMvcRegistrationsAdapter;
 import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
 
 import javax.annotation.Resource;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * Spring MVC 配置
@@ -60,6 +65,33 @@ public class SpringMvcConfig {
 				DefaultRequestMappingHandlerAdapter adapter = new DefaultRequestMappingHandlerAdapter();
 				adapter.setAutoView(true);
 				return adapter;
+			}
+		};
+	}
+	
+	/**
+	 * 修改MetaInfTldSources加载Taglib规则 - 默认加载/WEB-INF/lib下面的jar <br/>
+	 * 集成Spring Boot后没有WEB-INF目录，所以找不到原有的tld文件，修改成{@link ClasspathMetaInfTldSource}
+	 * 来加载所有的tld文件
+	 * @return
+	 */
+	@Bean
+	public static BeanPostProcessor freeMarkerConfigPostProcessor() {
+		return new BeanPostProcessor() {
+			@Override
+			public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+				return bean;
+			}
+			
+			@Override
+			public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+				if (bean instanceof FreeMarkerConfig) {
+					FreeMarkerConfig config = (FreeMarkerConfig) bean;
+					//修改MetaInfTldSources加载Taglib规则 - 默认加载/WEB-INF/lib下面的jar
+					List<ClasspathMetaInfTldSource> tldSources = Collections.singletonList(new ClasspathMetaInfTldSource(Pattern.compile(".*")));
+					config.getTaglibFactory().setMetaInfTldSources(tldSources);
+				}
+				return bean;
 			}
 		};
 	}
