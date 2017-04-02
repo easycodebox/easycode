77f2074f7f383cdaa81915843cac9cce1bc155ac
WangXiaoJin <381954728@qq.com>
2017-04-02 22:35:03 +0800

    update

--- easycode-auth-backend/src/main/java/com/easycodebox/auth/backend/config/SpringMvcConfig.java
+++ easycode-auth-backend/src/main/java/com/easycodebox/auth/backend/config/SpringMvcConfig.java
@@ -1,6 +1,8 @@
 package com.easycodebox.auth.backend.config;
 
+import com.easycodebox.common.CommonProperties;
 import com.easycodebox.common.error.ErrorContext;
+import com.easycodebox.common.web.callback.Callbacks;
 import com.easycodebox.common.web.springmvc.DefaultRequestMappingHandlerAdapter;
 import com.easycodebox.common.web.springmvc.DefaultRequestMappingHandlerMapping;
 import freemarker.ext.jsp.TaglibFactory.ClasspathMetaInfTldSource;
@@ -8,16 +10,21 @@ import freemarker.template.SimpleHash;
 import freemarker.template.TemplateModelException;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.NotWritablePropertyException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.web.*;
 import org.springframework.context.annotation.*;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -104,6 +111,7 @@ public class SpringMvcConfig {
 					if (ec.getError() != null) {
 						attributes.put("code", ec.getError().getCode());
 						attributes.put("msg", ec.getError().getMsg());
+						attributes.put("data", ec.getError().getData());
 					}
 				}
 				return attributes;
@@ -119,9 +127,26 @@ public class SpringMvcConfig {
 	                                            List<ErrorViewResolver> errorViewResolvers) {
 		return new BasicErrorController(errorAttributes, serverProperties.getError(), errorViewResolvers) {
 			
+			@Autowired
+			private CommonProperties commonProperties;
+			
 			@RequestMapping(path = "/{status}", produces = "text/html")
 			public String status(@PathVariable String status) {
 				return "error/" + status;
+			}
+			
+			@Override
+			@RequestMapping(produces = "text/html")
+			public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
+				if(request.getParameter(commonProperties.getDialogReqKey()) != null) {
+					Map<String, Object> attributes = getErrorAttributes(request, isIncludeStackTrace(request, MediaType.TEXT_HTML));
+					String code = (String) attributes.get("code");
+					String msg = (String) attributes.get("msg");
+					Object data = attributes.get("data");
+					Callbacks.callback(Callbacks.none(code, msg, data), null, response);
+					return null;
+				}
+				return super.errorHtml(request, response);
 			}
 			
 		};
